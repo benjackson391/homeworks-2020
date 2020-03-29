26760bb516e96a9a78a868565be5321c504c37b8
Ben Jackson <ilyaksyonov@gmail.com>
2020-03-29 12:01:46 +0300

    Update HW02/Ilia_Aksenov/ilia_aksenov_hw_02_t_01.rb
    Co-Authored-By: Vlad Hilko <vladislav.spawn@gmail.com>

--- HW02/Ilia_Aksenov/ilia_aksenov_hw_02_t_01.rb
+++ HW02/Ilia_Aksenov/ilia_aksenov_hw_02_t_01.rb
@@ -1,6 +1,6 @@
 def parse(filepath)
   File.readlines(filepath, chomp: true)
-      .find { |i| i.downcase.include? 'error' } || ''
+      .find { |i| i.downcase.include? 'error' }.to_s
 end
 
 puts 'This script can find errors in log file. Please, enter filepath: '
